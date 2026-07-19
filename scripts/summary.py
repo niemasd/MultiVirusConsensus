ed8a7b0554f543a8ff1f1505e20858e5a094173a
Niema Moshiri <niemamoshiri@gmail.com>
2026-07-19 10:19:31 -0700

    Added position coverage percentage

--- scripts/summary.py
+++ scripts/summary.py
@@ -19,13 +19,16 @@ HEADER = [
     'bases_total',
     f'bases_q{MIN_BASE_QUAL}',
     'reads_mapped',
+    'reads_mapped_prop',
     'reference_length',
     f'positions_cov>={MIN_COVERAGE}',
+    f'positions_cov>={MIN_COVERAGE}_prop',
 ]
 
 # cached values
 BAM_STATS = dict()
 REF_LENS = dict()
+POS_COV_METRICS = dict()
 
 # calculate multiple statistics from a MVC output BAM in a single pass over the alignments
 def calc_bam_stats(bam_path):
@@ -52,6 +55,7 @@ def calc_bam_stats(bam_path):
                     BAM_STATS['reads_mapped'][aln.reference_name] = 0
                 BAM_STATS['reads_mapped'][aln.reference_name] += 1
     BAM_STATS['reads_total'] = BAM_STATS['reads_unmapped'] + sum(BAM_STATS['reads_mapped'].values())
+    BAM_STATS['reads_mapped_prop'] = {k : v/BAM_STATS['reads_total'] for k, v in BAM_STATS['reads_mapped'].items()}
 
 # calculate reference lengths
 def calc_ref_lens(refs_path):
@@ -70,23 +74,35 @@ def calc_ref_lens(refs_path):
         REF_LENS[ID] = seq_len
     return REF_LENS
 
+# calculate position coverage metrics
+def calc_pos_cov_metrics(out_path):
+    POS_COV_METRICS[f'positions_cov>={MIN_COVERAGE}'] = dict()
+    for path in out_path.glob('*.poscounts.tsv'):
+        ref = path.name.replace('.poscounts.tsv','').strip()
+        with open(path, 'rt') as f:
+            POS_COV_METRICS[f'positions_cov>={MIN_COVERAGE}'][ref] = sum(1 for line_num, line in enumerate(f) if line_num != 0 and int(line.split()[-1]) >= MIN_COVERAGE)
+    if len(REF_LENS) == 0:
+        calc_ref_lens(out_path / 'references.fas')
+    POS_COV_METRICS[f'positions_cov>={MIN_COVERAGE}_prop'] = {k : v/REF_LENS[k] for k, v in POS_COV_METRICS[f'positions_cov>={MIN_COVERAGE}'].items()}
+
 # get the value of a given column for a given reference genome
 def get_value(out_path, ref, col):
     if len(BAM_STATS) == 0:
         calc_bam_stats(out_path / 'reads.bam')
     if len(REF_LENS) == 0:
         calc_ref_lens(out_path / 'references.fas')
+    if len(POS_COV_METRICS) == 0:
+        calc_pos_cov_metrics(out_path)
     ref = ref.strip().upper()
     col = col.strip().lower()
     if col == 'reference':
         return ref
     elif col == 'reference_length':
         return REF_LENS[ref]
-    elif col == f'positions_cov>={MIN_COVERAGE}':
-        with open(out_path / f'{ref}.poscounts.tsv', 'rt') as poscounts_f:
-            return sum(1 for line_num, line in enumerate(poscounts_f) if line_num != 0 and int(line.split()[-1]) >= MIN_COVERAGE)
-    elif col == 'reads_mapped':
-        return BAM_STATS['reads_mapped'][ref]
+    elif col.startswith('positions'):
+        return POS_COV_METRICS[col][ref]
+    elif col.startswith('reads_mapped'):
+        return BAM_STATS[col][ref]
     elif col in BAM_STATS:
         return BAM_STATS[col]
     else:
